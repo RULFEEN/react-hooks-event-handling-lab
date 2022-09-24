3632e63d3e68fd9370f17e8eadefba1b16ff2ff1
Victor Mayi <victormay.i.797@gmail.com>
2022-09-24 21:58:19 +0300

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,30 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+
+function EyesOnMe() {
+
+    function handleFocus(event) {
+        event.preventDefault();
+        console.log("Good!");
+    }
+    
+    function handleBlur(event) {
+        event.preventDefault();
+        console.log("Hey! Eyes on me!");
+    }
+    
+    return (
+        <>
+            <form>
+                <button 
+                    onFocus={handleFocus} 
+                    onBlur={handleBlur} 
+                    onClick={(event) => event.preventDefault()}>
+                    Eyes on me
+                </button>
+            </form>
+        </>
+    )
+}
+
+export default EyesOnMe;
